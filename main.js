c6e04a8bb5a87c64134194409caeeb7d93228b8b
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-10 10:19:13 +0100

    end

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ const chars = 'ABCDEFGIJK0123456789';
 const btn = document.querySelector('button');
 const section = document.querySelector('section');
 
-const codesNumber = 1000;
-const charsNumber = 12;
+const codeNumber = 1000;
+const charsNumber = 10;
 
 const codesGeneretor = () => {
- for (let i = 0; i < codesNumber; i++) {
-  let code = "";
-  for (let i = 0; i < charsNumber; i++) {
-   const index = Math.floor(Math.random() * 20)
-   code += chars[index];
-  }
-  const div = document.createElement('div');
-  div.textContent = code;
-  section.appendChild(div)
- }
+    for (let i = 0; i < codeNumber; i++) {
+        let code = "";
+        for (let i = 0; i < charsNumber; i++) {
+            // code += "1";
+            const index = Math.floor(Math.random() * 20)
+            code += chars[index];
+        }
+        const div = document.createElement('div');
+        div.textContent = code;
+        section.appendChild(div);
+    }
 }
 
-btn.addEventListener('click', codesGeneretor)
+btn.addEventListener('click', codesGeneretor);
